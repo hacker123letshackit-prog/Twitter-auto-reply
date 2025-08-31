4c551f9b5db80a8e732f3c0149839bce9dc14c6c
hacker123letshackit-prog <hacker123letshackit@gmail.com>
2025-08-31 20:20:20 +0530

    Update main.py

--- Twitter-auto-reply-main/main.py
+++ Twitter-auto-reply-main/main.py
@@ -2,8 +2,12 @@ import tweepy
 import time
 
 # Replace with your keys
-auth = tweepy.OAuthHandler("7EPPcZzplJA25sKRAaNqMdMwD", "dg0UekFizUj8sW1l28O3QSzf8FR3j9iOjUnTzSnuxrpwCMsyDk")
-auth.set_access_token("1962147919429259265-kM1FybPbeBJr0s9xZShqQlChpesITf", "YLMZPxHMqKTT1KtBMx0M0ppM20AS22C27Dh5pdYYQKleZ")
+auth = tweepy.OAuth1UserHandler(
+    "YOUR_API_KEY_HERE",
+    "YOUR_API_SECRET_HERE",
+    "YOUR_ACCESS_TOKEN_HERE",
+    "YOUR_ACCESS_TOKEN_SECRET_HERE"
+)
 api = tweepy.API(auth)
 
 # Customize your search terms here
@@ -13,10 +17,15 @@ search_terms = "#India OR politics"
 reply_message = "This is a reply from my Gemini bot!"
 
 while True:
-    for sn in tweepy.Cursor(api.search, q=search_terms).items(1):
+    # Updated search method
+    tweets = api.search_tweets(q=search_terms, count=1)
+    for tweet in tweets:
         try:
-            api.update_status("@" + sn.user.screen_name + " " + reply_message, in_reply_to_status_id=sn.id)
-            print("Replied to @" + sn.user.screen_name)
+            api.update_status(
+                f"@{tweet.user.screen_name} {reply_message}",
+                in_reply_to_status_id=tweet.id
+            )
+            print(f"Replied to @{tweet.user.screen_name}")
         except tweepy.TweepError as e:
             print(e.reason)
-    time.sleep(60)  # Wait 1 minute between checks
+    time.sleep(60)
